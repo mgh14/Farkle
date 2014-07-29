6de8fa5c7fdd266bd5b87d84b1f6018ce68bac11
Matt <chasing314@gmail.com>
2014-07-28 23:28:34 -0600

    Revert "Remove static modifier"
    This reverts commit a038c5aa5677b7df1c20619c6185880f417aaa5b.

--- src/main/engine/properties/PropertiesManager.java
+++ src/main/engine/properties/PropertiesManager.java
@@ -4,40 +4,37 @@ import java.util.HashMap;
 
 public class PropertiesManager {
 
-    private HashMap<String, String> properties = new HashMap<String, String>();
-    private DieValueEvaluator dieEvaluator;
+    private static HashMap<String, String> properties = new HashMap<String, String>();
+    private static DieValueEvaluator dieEvaluator;
 
-    private final String CONFIG_FILENAME_PROP_NAME = "configFile";
-    private final String NUM_PLAYERS_PROP_NAME = "numPlayers";
-    private final String MIN_DIE_VAL_PROP_NAME = "minVal";
-    private final String MAX_DIE_VAL_PROP_NAME = "maxVal";
-    private final String NUM_DICE_PROP_NAME = "numDice";
-    private final String REQ_POINTS_FOR_WIN_PROP_NAME = "reqPointsForWin";
+    private static final String CONFIG_FILENAME_PROP_NAME = "configFile";
+    private static final String NUM_PLAYERS_PROP_NAME = "numPlayers";
+    private static final String MIN_DIE_VAL_PROP_NAME = "minVal";
+    private static final String MAX_DIE_VAL_PROP_NAME = "maxVal";
+    private static final String NUM_DICE_PROP_NAME = "numDice";
+    private static final String REQ_POINTS_FOR_WIN_PROP_NAME = "reqPointsForWin";
 
-    public final int DEFAULT_NUM_PLAYERS = 2;
-    public final int DEFAULT_MIN_DIE_VALUE = 1;
-    public final int DEFAULT_MAX_DIE_VALUE = 6;
-    public final int DEFAULT_NUM_DICE = 6;
-    public final int DEFAULT_REQ_POINTS_FOR_WIN = 10000;
+    public static final int DEFAULT_NUM_PLAYERS = 2;
+    public static final int DEFAULT_MIN_DIE_VALUE = 1;
+    public static final int DEFAULT_MAX_DIE_VALUE = 6;
+    public static final int DEFAULT_NUM_DICE = 6;
+    public static final int DEFAULT_REQ_POINTS_FOR_WIN = 10000;
 
-    public final String DEFAULT_CONFIG_FILENAME = "config.xml";
+    public static final String DEFAULT_CONFIG_FILENAME = "config.xml";
 
-    public void setDieEvaluator(DieValueEvaluator evaluator) {
+    public static void setDieEvaluator(DieValueEvaluator evaluator) {
         dieEvaluator = evaluator;
     }
 
-    private void resetPropertiesMap() {
+    private static void resetPropertiesMap() {
         properties = new HashMap<String, String>();
     }
 
-    public void loadDefaultConfig() {
-        resetPropertiesMap();
-        properties.put(MIN_DIE_VAL_PROP_NAME, "here");
-        System.out.println("here");
+    public static void loadDefaultConfig() {
         //loadConfigFile(defaultFileName)
     }
 
-    public void loadConfigFile(String configFile) {
+    public static void loadConfigFile(String configFile) {
         resetPropertiesMap();
 
         /*//Load and Parse the XML document
@@ -84,27 +81,27 @@ public class PropertiesManager {
         properties.put(CONFIG_FILENAME_PROP_NAME, configFile);
     }
 
-    public int getNumPlayers() {
+    public static int getNumPlayers() {
         return Integer.getInteger(properties.get(NUM_PLAYERS_PROP_NAME));
     }
 
-    public int getMinDieValue() {
+    public static int getMinDieValue() {
         return Integer.getInteger(properties.get(MIN_DIE_VAL_PROP_NAME));
     }
 
-    public int getMaxDieValue() {
+    public static int getMaxDieValue() {
         return Integer.getInteger(properties.get(MAX_DIE_VAL_PROP_NAME));
     }
 
-    public int getNumDice() {
+    public static int getNumDice() {
         return Integer.getInteger(properties.get(NUM_DICE_PROP_NAME));
     }
 
-    public int getPointsReqForWin() {
+    public static int getPointsReqForWin() {
         return Integer.getInteger(properties.get(REQ_POINTS_FOR_WIN_PROP_NAME));
     }
 
-    public void verifyDieValueIsValid(int value) {
+    public static void verifyDieValueIsValid(int value) {
         dieEvaluator.verifyDieValueIsValid(getMinDieValue(), getMaxDieValue(), value);
     }
 
