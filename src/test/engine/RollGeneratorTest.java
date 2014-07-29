abcd01e08de5c648f1dfa17a6a108b47ecef65f9
mgh14 <chasing314@gmail.com>
2014-07-29 13:21:54 -0600

    add roll generator test

--- src/test/engine/RollGeneratorTest.java
+++ src/test/engine/RollGeneratorTest.java
@@ -1,5 +1,14 @@
 package test.engine;
 
+import main.engine.RollGenerator;
+import org.testng.annotations.Test;
+
 public class RollGeneratorTest {
 
+  private RollGenerator generator;
+
+  @Test
+  public void test() {
+
+  }
 }
