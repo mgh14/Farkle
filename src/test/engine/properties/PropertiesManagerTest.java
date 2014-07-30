2bea6564b029effefe53c5c8540d5947a13f551c
mgh14 <chasing314@gmail.com>
2014-07-30 16:39:43 -0600

    get rid of unneeded tests

--- src/test/engine/properties/PropertiesManagerTest.java
+++ src/test/engine/properties/PropertiesManagerTest.java
@@ -2,26 +2,4 @@ package test.engine.properties;
 
 public class PropertiesManagerTest {
 
-      /*@Test
-    public void defaultMinValIsOne() {
-        assertEquals(new Roll().getMinValue(), 1);
-    }
-
-    @Test
-    public void defaultMaxValIsSix() {
-        assertEquals(new Roll().getMaxValue(), 6);
-    }
-
-        @Test
-    public void testEqualsWithDifferentMinVals() {
-        assertNotEquals(TEST_ROLL, new Roll(TEST_ROLL.getMinValue() - 1,
-                TEST_ROLL.getMaxValue(), TEST_ROLL.getRollValue()));
-    }
-
-    @Test
-    public void testEqualswithDifferentMaxVals() {
-        assertNotEquals(TEST_ROLL, new Roll(TEST_ROLL.getMinValue(),
-                TEST_ROLL.getMaxValue() - 1, TEST_ROLL.getRollValue()));
-    }
-    */
 }
