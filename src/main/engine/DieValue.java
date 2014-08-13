d93bdd72c3c9f23cd5b1d1bb75016fba77394f73
Matt <chasing314@gmail.com>
2014-08-13 00:10:56 -0600

    Rename Roll class to DieValue, collapse logic

---
+++ src/main/engine/DieValue.java
@@ -0,0 +1,31 @@
+package main.engine;
+
+import main.engine.properties.PropertiesManager;
+
+public class DieValue {
+	
+	private int value;
+	
+	public DieValue() {
+		setDieValue(PropertiesManager.DEFAULT_MAX_DIE_VALUE);
+	}
+	
+	public DieValue(int ROLL_VALUE) {
+		setDieValue(ROLL_VALUE);
+	}
+	
+	public void setDieValue(int newVal) {
+		PropertiesManager.verifyDieValueIsValid(newVal);
+        value = newVal;
+	}
+	
+	public int getDieValue() {
+		return value;
+	}
+
+  @Override
+  public boolean equals(Object other) {
+    return !(other == null || !(other instanceof DieValue)) &&
+            getDieValue() == ((DieValue) other).getDieValue();
+  }
+}
--- src/main/engine/Roll.java
+++ src/main/engine/Roll.java
@@ -1,34 +1,63 @@
 package main.engine;
 
-import main.engine.properties.PropertiesManager;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Roll {
+  
+	private Collection<DieValue> diceVals;
+	private int MAX_NUM_DICE;
 	
-	private int value;
+	private final int DEFAULT_MAX_NUM_DICE = 6;
 	
-	public Roll() {
-		setRollValue(PropertiesManager.DEFAULT_MAX_DIE_VALUE);
+	Roll() {
+		initializeDiceRoll(new ArrayList<DieValue>());
 	}
 	
-	public Roll(int ROLL_VALUE) {
-		setRollValue(ROLL_VALUE);
-	}
-	
-	public void setRollValue(int newVal) {
-		PropertiesManager.verifyDieValueIsValid(newVal);
-    value = newVal;
-	}
-	
-	public int getRollValue() {
-		return value;
+	Roll(DieValue[] diceRolls) {
+		verifyOnlyNDice(diceRolls);
 	}
 
-  @Override
-  public boolean equals(Object other) {
-    if(other == null || !(other instanceof Roll)) {
-      return false;
+    private void setNumDice(int numDice) {
+        if(numDice < 1) {
+            throw new IllegalArgumentException("Number of dice must be at least one");
+        }
+
+        MAX_NUM_DICE = numDice;
     }
 
-    return getRollValue() == ((Roll) other).getRollValue();
-  }
+    public Collection<DieValue> getDiceVals() {
+        List<DieValue> valsCopy = new LinkedList<DieValue>();
+        valsCopy.addAll(diceVals);
+
+        return valsCopy;
+    }
+
+	private void initializeDiceRole(int maxNumDice, Collection<DieValue> diceValues) {
+		setNumDice(maxNumDice);
+
+        diceVals = (diceValues != null) ? diceValues : new ArrayList<DieValue>();
+		diceKept = new LinkedList<DieValue>();
+	}
+	
+	private void initializeDiceRoll(Collection<DieValue> diceRolls) {
+		initializeDiceRole(DEFAULT_MAX_NUM_DICE, diceRolls);
+	}
+	
+	private void verifyOnlyNDice(DieValue[] rolls) {
+		if(rolls.length > MAX_NUM_DICE) {
+			throw new IllegalArgumentException("Only max of " + MAX_NUM_DICE + " allowed");
+		}
+	}
+
+    public Collection<DiceScore> calculatePossibleScoresFromRoll() {
+        return calculatePossibleScoresFromRoll(diceVals);
+    }
+
+    public Collection<DiceScore> calculatePossibleScoresFromRoll(Collection<DieValue> rollVals) {
+        return null;
+    }
+
 }
