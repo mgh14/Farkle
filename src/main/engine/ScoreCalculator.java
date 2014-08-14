63a328dcf4cd72b8542f27d880bc8f692b96b363
hessm88 <mhess@familysearch.org>
2014-08-14 13:52:16 -0600

    Add scoring logic for a given roll

--- src/main/engine/ScoreCalculator.java
+++ src/main/engine/ScoreCalculator.java
@@ -2,62 +2,74 @@ package main.engine;
 
 import main.engine.properties.PropertiesManager;
 
-import java.util.Collection;
 import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
 
 public class ScoreCalculator {
 
-    public static final int NUM_POINTS_FOR_ONE = 100;
-    public static final int NUM_POINTS_FOR_FIVE = 50;
+  public static final int NUM_POINTS_FOR_ONE = 100;
+  public static final int NUM_POINTS_FOR_FIVE = 50;
 
-    public ScoreCalculator() {}
+  public ScoreCalculator() {}
 
-    public int calculateRollScore(Roll roll) {
-        final HashMap<Integer, Integer> counts = assignDieValueCounts(roll);
+  public int calculateRollScore(Roll roll) {
+      final HashMap<Integer, Integer> counts = assignDieValueCounts(roll);
 
-        int score = 0;
+      int score = 0;
 
-        // n number of dice of same numeric value (e.g. two 5's or 4 6's)
-        for(int i= PropertiesManager.getMinDieValue(); i<=PropertiesManager.getMaxDieValue(); i++)
-            score += scoreNDiceOfSameValue(counts, i);
+      // n number of dice of same numeric value (e.g. two 5's or 4 6's)
+      for(int i= PropertiesManager.getMinDieValue(); i<=PropertiesManager.getMaxDieValue(); i++)
+          score += scoreNDiceOfSameValue(counts, i);
 
-        // straight (1 - 6)
-            score += scoreStraightRun(counts);
+      // straight (1 - 6)
+          score += scoreStraightRun(counts);
 
 
-        return score;
+      return score;
+  }
+
+  private HashMap<Integer, Integer> assignDieValueCounts(Roll roll) {
+    HashMap<Integer, Integer> counts = new HashMap<Integer, Integer>();
+    for(int i=PropertiesManager.getMinDieValue(); i<=PropertiesManager.getMaxDieValue(); i++) {
+      counts.put(i, 0);
     }
 
-    private HashMap<Integer, Integer> assignDieValueCounts(Roll roll) {
-        HashMap<Integer, Integer> counts = new HashMap<Integer, Integer>();
-        for(DieValue dieValue : roll.getDiceVals()) {
-            int mine = dieValue.getDieValue();
-            if(counts.containsKey(mine)) {
-                counts.put(mine, counts.get(mine) + 1);
-            }
-            else {
-                counts.put(mine, 1);
-            }
-        }
-
-        return counts;
+    for(DieValue dieValue : roll.getDiceVals()) {
+        int mine = dieValue.getDieValue();
+        counts.put(mine, counts.get(mine) + 1);
     }
 
-    private int scoreNDiceOfSameValue(HashMap<Integer, Integer> counts, int dieValue) {
-        return 0;
+      return counts;
+  }
+
+  private int scoreNDiceOfSameValue(HashMap<Integer, Integer> counts, int dieValue) {
+      return 0;
+  }
+
+  private int scoreThreeSetsOfTwo(HashMap<Integer, Integer> counts) {
+    return scoreNSetsOfNDice(counts, 3, 2, 5000);
+  }
+
+  private int scoreTwoSetsOfThree(HashMap<Integer, Integer> counts) {
+    return scoreNSetsOfNDice(counts, 2, 3, 10000);
+  }
+
+  private int scoreStraightRun(HashMap<Integer, Integer> counts) {
+    return scoreNSetsOfNDice(counts, 6, 1, 20000);
+  }
+
+  private int scoreNSetsOfNDice(HashMap<Integer, Integer> counts, int numSets, int numDiceInEachSet, int pointsToGain) {
+    int numSetsOfTwo = 0;
+    for(Integer key : counts.keySet()) {
+      if(counts.get(key) >= numDiceInEachSet) {
+        numSetsOfTwo++;
+      }
     }
 
-    private int scoreStraightRun(HashMap<Integer, Integer> counts) {
-        for(int i=PropertiesManager.getMinDieValue(); i<= PropertiesManager.getMaxDieValue(); i++) {
-            if (!counts.containsKey(i) || counts.get(i) != 1) {
-                return 0;
-            }
-        }
-
-        return 0;
+    if(numSetsOfTwo >= numSets) {
+      return pointsToGain;
     }
 
-    //private int score
+    return 0;
+  }
+
 }
