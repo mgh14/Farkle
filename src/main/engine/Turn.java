2059dc9e444aa79089ee4ba6e4c46ba485345ff1
hessm88 <mhess@familysearch.org>
2014-08-15 13:59:47 -0600

    Make private members lists for rolls and scores

--- src/main/engine/Turn.java
+++ src/main/engine/Turn.java
@@ -1,26 +1,40 @@
 package main.engine;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Turn {
 
-    final private Roll roll;
-    final private RollScore score;
+    final private List<Roll> roll;
+    final private List<RollScore> scores;
     final private Player player;
 
-    public Turn(Roll playerRoll, RollScore playerScore, Player playerWhoTookTurn) {
-        roll = playerRoll;
-        score = playerScore;
-        player = playerWhoTookTurn;
-    }
+  public Turn(Player playerWhoTakesTurn) {
+    roll = new LinkedList<Roll>();
+    scores = new LinkedList<RollScore>();
+    player = playerWhoTakesTurn;
+  }
 
-    public Roll getRoll() {
+    /*public Turn(Roll playerRoll, RollScore playerScore, Player playerWhoTookTurn) {
+        roll = playerRoll;
+
+
+        scores = playerScore;
+
+
+
+        player = playerWhoTookTurn;
+    }*/
+
+    /*public Roll getRoll() {
         return roll;
     }
 
     public RollScore getDiceScore() {
-        return score;
+        return scores;
     }
 
     public Player getPlayer() {
         return player;
-    }
+    }*/
 }
