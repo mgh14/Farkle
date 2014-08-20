5c6bc679bef992a336b56bbdb05c67af90f98eac
MHess <chasing314@gmail.com>
2014-08-19 21:14:09 -0600

    Reconstruct roll object

--- src/main/engine/Roll.java
+++ src/main/engine/Roll.java
@@ -7,6 +7,7 @@ import java.util.*;
 public class Roll {
   
 	private List<DieValue> diceVals;
+    private RollScore rollScore;
 	private int MAX_NUM_DICE;
 	
 	public Roll() {
@@ -17,6 +18,48 @@ public class Roll {
 		initializeDiceRole(numDice, diceVals);
 	}
 
+    public void setDiceKept(List<DieValue> diceKept) {
+        rollScore = new RollScore(diceKept, new ScoreCalculator().calculateRollScore(this));
+    }
+
+    public int getScoreForRoll() {
+        return (rollScore != null) ? rollScore.getNumberOfPoints() : null;
+    }
+
+    public List<DieValue> getDiceVals() {
+        List<DieValue> valsCopy = new LinkedList<DieValue>();
+        valsCopy.addAll(diceVals);
+
+        return valsCopy;
+    }
+
+    public List<DieValue> getDiceKept() {
+        return (rollScore != null) ? rollScore.getDiceKept() : null;
+    }
+
+    public boolean canRollAgain(ScoreCalculator scoreCalc) {
+        if(scoreCalc == null) {
+            throw new IllegalArgumentException("Score calculator cant be null");
+        }
+
+        // if dice kept hasn't been initialized, the player's turn hasn't
+        // started and they can roll again.
+        List<DieValue> diceKept = getDiceKept();
+        if(diceKept == null) {
+            return true;
+        }
+
+        List<DieValue> leftoverDice = new LinkedList<DieValue>();
+        for(DieValue current : getDiceVals()) {
+            if(!diceKept.contains(current)) {
+                leftoverDice.add(current);
+            }
+        }
+
+        return scoreCalc.calculateRollScore(leftoverDice) > 0;
+    }
+
+
   private void setNumDice(int numDice) {
     if(numDice < PropertiesManager.DEFAULT_MIN_NUM_DICE) {
         throw new IllegalArgumentException("Number of dice must be at least one");
@@ -25,26 +68,20 @@ public class Roll {
     MAX_NUM_DICE = numDice;
   }
 
-  public List<DieValue> getDiceVals() {
-      List<DieValue> valsCopy = new LinkedList<DieValue>();
-      valsCopy.addAll(diceVals);
-
-      return valsCopy;
-  }
-
-	private void initializeDiceRole(int maxNumDice, List<DieValue> diceValues) {
+	private void initializeDiceRole(int maxNumDice, List<DieValue> diceVals) {
+        rollScore = null;
 		setNumDice(maxNumDice);
 
-    diceVals = (diceValues != null) ? diceValues : new ArrayList<DieValue>();
-    verifyOnlyNDice(diceVals);
+        this.diceVals = (diceVals != null) ? diceVals : new LinkedList<DieValue>();
+        verifyOnlyNDice(this.diceVals);
 	}
 	
-	private void initializeDiceRoll(List<DieValue> diceRolls) {
-		initializeDiceRole(PropertiesManager.DEFAULT_NUM_DICE, diceRolls);
+	private void initializeDiceRoll(List<DieValue> diceVals) {
+		initializeDiceRole(PropertiesManager.DEFAULT_NUM_DICE, diceVals);
 	}
 	
-	private void verifyOnlyNDice(List<DieValue> rolls) {
-		if(rolls.size() > MAX_NUM_DICE) {
+	private void verifyOnlyNDice(List<DieValue> diceVals) {
+		if(diceVals.size() > MAX_NUM_DICE) {
 			throw new IllegalArgumentException("Only max of " + MAX_NUM_DICE + " allowed");
 		}
 	}
