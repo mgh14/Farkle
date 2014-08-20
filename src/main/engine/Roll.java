18a5dce6da3f0d91c008f3eb5099aa0c07e47550
hessm88 <mhess@familysearch.org>
2014-08-20 14:28:45 -0600

    Fix unboxing issue; reorder methods

--- src/main/engine/Roll.java
+++ src/main/engine/Roll.java
@@ -7,7 +7,7 @@ import java.util.*;
 public class Roll {
   
 	private List<DieValue> diceVals;
-    private RollScore rollScore;
+  private RollScore rollScore;
 	private int MAX_NUM_DICE;
 	
 	public Roll() {
@@ -18,62 +18,62 @@ public class Roll {
 		initializeDiceRole(numDice, diceVals);
 	}
 
-    public void setDiceKept(List<DieValue> diceKept) {
-        rollScore = new RollScore(diceKept, new ScoreCalculator().calculateRollScore(this));
+  public Integer getScoreForRoll() {
+    return (rollScore != null) ? (Integer) rollScore.getNumberOfPoints() : null;
+  }
+
+  public List<DieValue> getDiceVals() {
+    List<DieValue> valsCopy = new LinkedList<DieValue>();
+    valsCopy.addAll(diceVals);
+
+    return valsCopy;
+  }
+
+  public void setDiceKept(List<DieValue> diceKept, int pointsGained, ScoreCalculator scoreCalc) {
+    rollScore = new RollScore(diceKept, pointsGained, scoreCalc);
+  }
+
+  public List<DieValue> getDiceKept() {
+    return (rollScore != null) ? rollScore.getDiceKept() : null;
+  }
+
+  public boolean canRollAgain(ScoreCalculator scoreCalc) {
+    if(scoreCalc == null) {
+        throw new IllegalArgumentException("Score calculator cant be null");
     }
 
-    public int getScoreForRoll() {
-        return (rollScore != null) ? rollScore.getNumberOfPoints() : null;
+    // if dice kept hasn't been initialized, the player's turn hasn't
+    // started and they can roll again.
+    List<DieValue> diceKept = getDiceKept();
+    if(diceKept == null) {
+        return true;
     }
 
-    public List<DieValue> getDiceVals() {
-        List<DieValue> valsCopy = new LinkedList<DieValue>();
-        valsCopy.addAll(diceVals);
-
-        return valsCopy;
+    List<DieValue> leftoverDice = new LinkedList<DieValue>();
+    for(DieValue current : getDiceVals()) {
+      if(!diceKept.contains(current)) {
+          leftoverDice.add(current);
+      }
     }
 
-    public List<DieValue> getDiceKept() {
-        return (rollScore != null) ? rollScore.getDiceKept() : null;
-    }
-
-    public boolean canRollAgain(ScoreCalculator scoreCalc) {
-        if(scoreCalc == null) {
-            throw new IllegalArgumentException("Score calculator cant be null");
-        }
-
-        // if dice kept hasn't been initialized, the player's turn hasn't
-        // started and they can roll again.
-        List<DieValue> diceKept = getDiceKept();
-        if(diceKept == null) {
-            return true;
-        }
-
-        List<DieValue> leftoverDice = new LinkedList<DieValue>();
-        for(DieValue current : getDiceVals()) {
-            if(!diceKept.contains(current)) {
-                leftoverDice.add(current);
-            }
-        }
-
-        return scoreCalc.calculateRollScore(leftoverDice) > 0;
-    }
+    return scoreCalc.calculateRollScore(leftoverDice) > 0;
+  }
 
 
   private void setNumDice(int numDice) {
     if(numDice < PropertiesManager.DEFAULT_MIN_NUM_DICE) {
-        throw new IllegalArgumentException("Number of dice must be at least one");
+      throw new IllegalArgumentException("Number of dice must be at least one");
     }
 
     MAX_NUM_DICE = numDice;
   }
 
 	private void initializeDiceRole(int maxNumDice, List<DieValue> diceVals) {
-        rollScore = null;
+    rollScore = null;
 		setNumDice(maxNumDice);
 
-        this.diceVals = (diceVals != null) ? diceVals : new LinkedList<DieValue>();
-        verifyOnlyNDice(this.diceVals);
+    this.diceVals = (diceVals != null) ? diceVals : new LinkedList<DieValue>();
+    verifyOnlyNDice(this.diceVals);
 	}
 	
 	private void initializeDiceRoll(List<DieValue> diceVals) {
