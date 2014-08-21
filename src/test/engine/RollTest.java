f945b0b39fb30ed8eaf7d78938b001bf2dc3e54a
hessm88 <mhess@familysearch.org>
2014-08-21 14:08:26 -0600

    fill in setDiceKept test methods

--- src/test/engine/RollTest.java
+++ src/test/engine/RollTest.java
@@ -2,17 +2,36 @@ package test.engine;
 
 import main.engine.DieValue;
 import main.engine.Roll;
+import main.engine.ScoreCalculator;
 import main.engine.properties.PropertiesManager;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.LinkedList;
 import java.util.List;
 
+import static org.mockito.Matchers.anyList;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.testng.Assert.assertEquals;
 
 public class RollTest {
 
+  @Mock
+  private ScoreCalculator scoreCalc;
+
+  private final int[] TEST_ROLL_ARRAY = {1, 2, 3, 4, 5, 4};
+  private final List<DieValue> TEST_ROLL_LIST = getDiceVals(TEST_ROLL_ARRAY);
+  private final int TEST_ROLL_LIST_SCORE = ScoreCalculator.NUM_POINTS_FOR_ONE +
+    ScoreCalculator.NUM_POINTS_FOR_FIVE;
   private final int DEFAULT_NUM_DICE = PropertiesManager.DEFAULT_NUM_DICE;
+
+  @BeforeMethod
+  public void setUp() {
+    MockitoAnnotations.initMocks(this);
+  }
 
   @Test
   public void verifyDefaultDiceNumberInNoParamRollConstructor() {
@@ -42,10 +61,11 @@ public class RollTest {
 
   @Test
   public void verifyValidDieValuesSize() {
-    List<DieValue> legitimateVals = new LinkedList<DieValue>();
-    for (int i = 0; i < DEFAULT_NUM_DICE; i++) {
-      legitimateVals.add(new DieValue());
+    int[] diceVals = new int[DEFAULT_NUM_DICE];
+    for(int i=0; i<DEFAULT_NUM_DICE; i++) {
+      diceVals[i] = i + 1;
     }
+    List<DieValue> legitimateVals = getDiceVals(diceVals);
 
     List<DieValue> legitimateRoll = new Roll(DEFAULT_NUM_DICE, legitimateVals).getDiceVals();
     assertEquals(legitimateRoll.size(), legitimateVals.size());
@@ -54,29 +74,62 @@ public class RollTest {
     }
   }
 
-  @Test
+  @Test(expectedExceptions = IllegalArgumentException.class)
   public void testSetDiceKeptWithNullDiceKept() {
-
+    Roll roll = new Roll(DEFAULT_NUM_DICE, TEST_ROLL_LIST);
+    roll.setDiceKept(null, 0, scoreCalc);
   }
 
-  @Test
+  @Test(expectedExceptions = IllegalArgumentException.class)
   public void testSetDiceKeptWithDiceKeptBiggerThanNumDice() {
+    Roll roll = new Roll(DEFAULT_NUM_DICE, TEST_ROLL_LIST);
 
+    int[] diceKeptVals = new int[DEFAULT_NUM_DICE + 1];
+    for(int i=0; i<DEFAULT_NUM_DICE + 1; i++) {
+      diceKeptVals[i] = PropertiesManager.getMaxDieValue() - 1;
+    }
+
+    roll.setDiceKept(getDiceVals(diceKeptVals), 0, scoreCalc);
   }
 
-  @Test
+  @Test(expectedExceptions = IllegalArgumentException.class)
   public void testSetDiceKeptWithNegativePointsGained() {
-
+    Roll roll = new Roll(DEFAULT_NUM_DICE, TEST_ROLL_LIST);
+    roll.setDiceKept(TEST_ROLL_LIST, -1, scoreCalc);
   }
 
-  @Test
+  @Test(expectedExceptions = IllegalArgumentException.class)
   public void testSetDiceKeptWithNullScoreCalc() {
+    Roll roll = new Roll(DEFAULT_NUM_DICE, TEST_ROLL_LIST);
+    roll.setDiceKept(TEST_ROLL_LIST, 0, null);
+  }
 
+  @Test(expectedExceptions = IllegalArgumentException.class)
+  public void testSetDiceKeptWithUnmatchingDiceKeptScoreAndPointsGained() {
+    when(scoreCalc.calculateRollScore(anyList())).thenReturn(TEST_ROLL_LIST_SCORE);
+
+    Roll roll = new Roll(DEFAULT_NUM_DICE, TEST_ROLL_LIST);
+    roll.setDiceKept(TEST_ROLL_LIST, TEST_ROLL_LIST_SCORE - 1, scoreCalc);
+    verify(scoreCalc).calculateRollScore(anyList());
   }
 
   @Test
-  public void testSetDiceKeptWithUnmatchingDiceKeptScoreAndPointsGained() {
+  public void testSetDiceKeptWithSomeDiceKept() {
+    when(scoreCalc.calculateRollScore(anyList())).thenReturn(TEST_ROLL_LIST_SCORE);
 
+    Roll roll = new Roll(DEFAULT_NUM_DICE, getDiceVals(TEST_ROLL_ARRAY));
+    roll.setDiceKept(getDiceVals(TEST_ROLL_ARRAY[0], TEST_ROLL_ARRAY[4]),
+      TEST_ROLL_LIST_SCORE, scoreCalc);
+    verify(scoreCalc).calculateRollScore(anyList());
+  }
+
+  @Test
+  public void testSetDiceKeptWithAllDiceKept() {
+    when(scoreCalc.calculateRollScore(anyList())).thenReturn(TEST_ROLL_LIST_SCORE);
+
+    Roll roll = new Roll(DEFAULT_NUM_DICE, getDiceVals(TEST_ROLL_ARRAY));
+    roll.setDiceKept(getDiceVals(TEST_ROLL_ARRAY), TEST_ROLL_LIST_SCORE, scoreCalc);
+    verify(scoreCalc).calculateRollScore(anyList());
   }
 
   @Test
@@ -119,7 +172,6 @@ public class RollTest {
 
   }
 
-
   @Test
   public void testKeptDiceGainNoPointsWithNullScoreCalc() {
 
@@ -133,6 +185,19 @@ public class RollTest {
   @Test
   public void testKeptDiceGainNoPointsWithDiceKeptThatDontGainPoints() {
 
+  }
+
+  private List<DieValue> getDiceVals(int ... diceVals) {
+    List<DieValue> diceValsList = new LinkedList<DieValue>();
+    if(diceVals == null) {
+      return diceValsList;
+    }
+
+    for (Integer dieVal : diceVals) {
+      diceValsList.add(new DieValue(dieVal));
+    }
+
+    return diceValsList;
   }
 
 }
