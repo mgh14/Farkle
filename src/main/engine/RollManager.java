2d1189d28906f990e4108651d48b585393156269
MHess <chasing314@gmail.com>
2014-08-23 09:53:24 -0600

    More work on roll manager

--- src/main/engine/RollManager.java
+++ src/main/engine/RollManager.java
@@ -7,46 +7,52 @@ import java.util.List;
 
 public class RollManager {
 
-    private List<Turn> rolls;
-    private Turn currentTurn;
+  private List<Turn> turns;
+  private Turn currentTurn;
 
-    private DieValueGenerator generator;
-    private ScoreCalculator scoreCalc;
+  private DieValueGenerator generator;
+  private ScoreCalculator scoreCalc;
 
-    public RollManager() {
-        rolls = new LinkedList<Turn>();
-        setCurrentTurn(null);
+  public RollManager(DieValueGenerator dieValueGenerator, ScoreCalculator scoreCalculator) {
+    turns = new LinkedList<Turn>();
+    setCurrentTurn(null);
 
-        generator = new DieValueGenerator();
-        scoreCalc = new ScoreCalculator();
+    generator = dieValueGenerator;
+    scoreCalc = scoreCalculator;
+  }
+
+  public RollManager() {
+    new RollManager(new DieValueGenerator(), new ScoreCalculator());
+  }
+
+  private void setCurrentTurn(Turn turn) {
+    currentTurn = turn;
+  }
+
+  private Roll getRoll(int numDice) {
+    return new Roll(PropertiesManager.getNumDice(), generator.getDieValues(PropertiesManager.getMinDieValue(),
+      PropertiesManager.getMaxDieValue(), numDice));
+  }
+
+  public int keepDieValues(int... dieValIndices) {
+    //currentTurn.getRolls().get(1)
+  }
+
+  public Roll getNextRoll() {
+    if (!canRollAgain()) {
+      throw new IllegalStateException("no more rolls allowed");
     }
 
-    private void setCurrentTurn(Turn turn) {
-        currentTurn = turn;
-    }
+    //int size = currentRoll.getDiceVals().size();
 
-    private Roll getRoll(int numDice) {
-        return new Roll(PropertiesManager.getNumDice(), generator.getDieValues(PropertiesManager.getMinDieValue(),
-                PropertiesManager.getMaxDieValue(), numDice));
-    }
+    //return getRoll();
+    //currentTurn.
+    return null;
+  }
 
-/*    public int keepDieValues(int ... dieVals) {
-
-    }
-
-    public Roll getNextRoll() {
-        if(!canRollAgain()) {
-            throw new IllegalStateException("no more rolls allowed");
-        }
-
-        int size = currentRoll.getDiceVals().size();
-
-        return getRoll();
-    }*/
-
-    public boolean canRollAgain() {
-        return currentTurn == null || currentTurn.canRollAgain(scoreCalc);
-    }
+  public boolean canRollAgain() {
+    return currentTurn == null || currentTurn.canRollAgain(scoreCalc);
+  }
 
 /*    public void addRoll(Roll playerRoll) {
         rolls.add(playerRoll);
@@ -54,8 +60,12 @@ public class RollManager {
 
 */
 
- /*   public void undoLastTurn() {
-        turns.remove(turns.size() - 1);
-    }*/
+  public void undoLastTurn() {
+    if (currentTurn != null) {
+      throw new IllegalStateException("Cant undo turn while turn is being taken");
+    }
+
+    turns.remove(turns.size() - 1);
+  }
 
 }
