537bd65cb06b591798f285d52b0bf11a6602fcc7
MHess <chasing314@gmail.com>
2014-08-26 21:40:20 -0600

    Roll manager functionality and some of the tests

--- src/main/engine/RollManager.java
+++ src/main/engine/RollManager.java
@@ -14,6 +14,99 @@ public class RollManager {
   private ScoreCalculator scoreCalc;
 
   public RollManager(DieValueGenerator dieValueGenerator, ScoreCalculator scoreCalculator) {
+    initializeRollManager(dieValueGenerator, scoreCalculator);
+  }
+
+  public RollManager() {
+    new RollManager(new DieValueGenerator(), new ScoreCalculator());
+  }
+
+  public boolean turnInPlay() {
+    return currentTurn != null;
+  }
+
+  public Roll beginTurn(Player player) {
+    if (player == null) {
+      throw new IllegalArgumentException("Player cant be null");
+    }
+
+    setCurrentTurn(new Turn(player));
+
+    final Roll firstRoll = getRoll(PropertiesManager.getNumDice());
+    currentTurn.addRoll(firstRoll);
+
+    return firstRoll;
+  }
+
+  public boolean canRollAgain() {
+    return turnInPlay() && currentTurn.canRollAgain(scoreCalc);
+  }
+
+  public Roll getNextRoll() {
+    if (!turnInPlay()) {
+      throw new IllegalStateException("Cant get next roll until turn is started");
+    }
+
+    if (!canRollAgain()) {
+      throw new IllegalStateException("no more rolls allowed");
+    }
+
+    int size = currentTurn.getLastRoll().getDiceVals().size();
+    Roll nextRoll = getRoll(PropertiesManager.getNumDice() - size);
+
+    currentTurn.addRoll(nextRoll);
+
+    return nextRoll;
+  }
+
+  public void addRoll(Roll playerRoll) {
+    if (playerRoll == null) {
+      throw new IllegalArgumentException("Roll cant be null");
+    }
+
+    currentTurn.addRoll(playerRoll);
+  }
+
+  public int keepDieValues(int... dieValIndices) {
+    if (currentTurn == null) {
+      throw new IllegalStateException("No turn has been initialized; cant keep dice");
+    }
+
+    List<DieValue> diceRolled = currentTurn.getLastRoll().getDiceVals();
+    List<DieValue> diceToKeep = new LinkedList<DieValue>();
+    for (int dieVal : dieValIndices) {
+      diceToKeep.add(diceRolled.get(dieVal));
+    }
+
+    currentTurn.setDiceKept(diceToKeep, scoreCalc.calculateRollScore(diceToKeep), scoreCalc);
+
+    return currentTurn.getLastRoll().getScoreForRoll();
+  }
+
+  public void undoLastTurn() {
+    if (turnInPlay()) {
+      throw new IllegalStateException("Cant undo turn while turn is being taken");
+    }
+
+    turns.remove(turns.size() - 1);
+  }
+
+  public Roll getLastRoll() {
+    if(!turnInPlay()) {
+      throw new IllegalStateException("no turn in play");
+    }
+
+    return currentTurn.getLastRoll();
+  }
+
+  // used for testing
+  public RollManager reset() {
+    initializeRollManager(new DieValueGenerator(), new ScoreCalculator());
+
+    return this;
+  }
+
+  private void initializeRollManager(DieValueGenerator dieValueGenerator, ScoreCalculator scoreCalculator) {
     turns = new LinkedList<Turn>();
     setCurrentTurn(null);
 
@@ -21,51 +114,14 @@ public class RollManager {
     scoreCalc = scoreCalculator;
   }
 
-  public RollManager() {
-    new RollManager(new DieValueGenerator(), new ScoreCalculator());
-  }
-
-  private void setCurrentTurn(Turn turn) {
-    currentTurn = turn;
-  }
-
   private Roll getRoll(int numDice) {
     return new Roll(PropertiesManager.getNumDice(), generator.getDieValues(PropertiesManager.getMinDieValue(),
       PropertiesManager.getMaxDieValue(), numDice));
   }
 
-  public int keepDieValues(int... dieValIndices) {
-    //currentTurn.getRolls().get(1)
-  }
-
-  public Roll getNextRoll() {
-    if (!canRollAgain()) {
-      throw new IllegalStateException("no more rolls allowed");
-    }
-
-    //int size = currentRoll.getDiceVals().size();
-
-    //return getRoll();
-    //currentTurn.
-    return null;
-  }
-
-  public boolean canRollAgain() {
-    return currentTurn == null || currentTurn.canRollAgain(scoreCalc);
-  }
-
-/*    public void addRoll(Roll playerRoll) {
-        rolls.add(playerRoll);
-    }
-
-*/
-
-  public void undoLastTurn() {
-    if (currentTurn != null) {
-      throw new IllegalStateException("Cant undo turn while turn is being taken");
-    }
-
-    turns.remove(turns.size() - 1);
+  // used only for testing outside this class (should be an internal method)
+  public void setCurrentTurn(Turn turn) {
+    currentTurn = turn;
   }
 
 }
---
+++ src/test/engine/RollManagerTest.java
@@ -0,0 +1,144 @@
+package test.engine;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import main.engine.DieValue;
+import main.engine.DieValueGenerator;
+import main.engine.Player;
+import main.engine.Roll;
+import main.engine.RollManager;
+import main.engine.ScoreCalculator;
+import main.engine.Turn;
+import main.engine.properties.PropertiesManager;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
+
+public class RollManagerTest {
+
+  private final int NUM_DICE_START = PropertiesManager.getNumDice();
+
+  @InjectMocks
+  private RollManager manager;
+
+  @Mock
+  private DieValueGenerator generator;
+
+  @Mock
+  private ScoreCalculator scoreCalc;
+
+  @Mock
+  private Player mockPlayer;
+
+  @BeforeMethod
+  public void setUp() {
+    MockitoAnnotations.initMocks(this);
+  }
+
+  @Test
+  public void testTurnInPlayBeforeTurnStarts() {
+    assertFalse(manager.turnInPlay());
+  }
+
+  @Test
+  public void testTurnInPlayAfterTurnStarts() {
+    manager.beginTurn(mockPlayer);
+    assertTrue(manager.turnInPlay());
+
+    manager.reset();
+  }
+
+  @Test(expectedExceptions = IllegalArgumentException.class)
+  public void testBeginTurnWithNullPlayer() {
+    manager.beginTurn(null);
+  }
+
+  @Test
+  public void testBeginLegitimateTurn() {
+    when(generator.getDieValues(anyInt(), anyInt(), anyInt())).thenReturn(getTestDieValueList(null, NUM_DICE_START));
+
+    final Roll roll = manager.beginTurn(mockPlayer);
+    assertTrue(manager.turnInPlay());
+    assertEquals(roll.getDiceVals().size(), NUM_DICE_START);
+    verify(generator).getDieValues(anyInt(), anyInt(), anyInt());
+
+    manager.reset();
+  }
+
+  @Test
+  public void testCanRollAgainWithNoTurnInPlay() {
+    assertFalse(manager.canRollAgain());
+  }
+
+  @Test
+  public void testCanRollAgainWithNoMoreRollsAvailable() {
+    final Turn mockTurn = mock(Turn.class);
+    when(mockTurn.canRollAgain(any(ScoreCalculator.class))).thenReturn(false);
+    manager.setCurrentTurn(mockTurn);   // this will put turn in play
+
+    assertFalse(manager.canRollAgain());
+    verify(mockTurn).canRollAgain(any(ScoreCalculator.class));
+
+    manager.reset();
+  }
+
+  @Test
+  public void testCanRollAgainWithMoreRollsAvailable() {
+    final Turn mockTurn = mock(Turn.class);
+    when(mockTurn.canRollAgain(any(ScoreCalculator.class))).thenReturn(true);
+    manager.setCurrentTurn(mockTurn);   // this will put turn in play as well
+
+    assertTrue(manager.canRollAgain());
+    verify(mockTurn).canRollAgain(any(ScoreCalculator.class));
+
+    manager.reset();
+  }
+
+
+
+
+  @Test(expectedExceptions = IllegalArgumentException.class)
+  public void testAddRollWithNullRoll() {
+    manager.addRoll(null);
+  }
+
+  @Test
+  public void testAddRollWithLegitimateRoll() {
+    final Turn mockTurn = mock(Turn.class);
+    manager.setCurrentTurn(mockTurn);
+
+    final Roll mockRoll = mock(Roll.class);
+    when(mockTurn.getLastRoll()).thenReturn(mockRoll);
+
+    manager.addRoll(mockRoll);
+    assertEquals(manager.getLastRoll(), mockRoll);
+    verify(mockTurn).getLastRoll();
+
+    manager.reset();
+  }
+
+
+
+
+  private List<DieValue> getTestDieValueList(DieValue dieValue, int numToAdd) {
+    List<DieValue> dieValues = new LinkedList<DieValue>();
+    for(int i=0; i<numToAdd; i++) {
+      dieValues.add(dieValue);
+    }
+
+    return dieValues;
+  }
+
+}
