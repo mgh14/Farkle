aafb03e15c059df802f4594b7889da81ac9561bc
MHess <chasing314@gmail.com>
2014-08-27 16:58:06 -0600

    Add some game functionality to driver

--- src/main/engine/driver/Farkle.java
+++ src/main/engine/driver/Farkle.java
@@ -1,10 +1,25 @@
 package main.engine.driver;
 
-import main.engine.properties.PropertiesManager;
+import main.engine.Player;
+import main.engine.PlayerManager;
+import main.engine.Roll;
+import main.engine.RollManager;
 
 public class Farkle {
 
     public static void main(String[] args) {
-        PropertiesManager.loadSettingsProfile("profiles/4playerProfile.xml");
+        //PropertiesManager.loadSettingsProfile("profiles/4playerProfile.xml");
+
+      RollManager rollManager = new RollManager();
+      PlayerManager playerManager = new PlayerManager();
+
+      Player me = new Player(1);
+      playerManager.addPlayer(me);
+
+      while(true) {
+
+        Roll roll = rollManager.beginTurn(playerManager.getCurrentPlayer());
+      }
+
     }
 }
