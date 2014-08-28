d960edaa2b2e82b13b76c3b44e7d326e8d4c7f88
hessm88 <mhess@familysearch.org>
2014-08-28 11:04:20 -0600

    Add methods to player manager; add shell for test

--- src/main/engine/PlayerManager.java
+++ src/main/engine/PlayerManager.java
@@ -8,40 +8,61 @@ import java.util.List;
 
 public class PlayerManager {
 
-    List<Player> players;
-    int currentPlayerIndex;
-    HashMap<Player, Integer> playerScores;
+  private List<Player> players;
+  private int currentPlayerIndex;
+  private HashMap<Player, Integer> playerScores;
 
-    public PlayerManager() {
-        players = new LinkedList<Player>();
-        playerScores = new HashMap<Player, Integer>();
+  private final int STARTING_PLAYER_INDEX = -1;
 
-      currentPlayerIndex = -1;
+  public PlayerManager() {
+    players = new LinkedList<Player>();
+    playerScores = new HashMap<Player, Integer>();
+
+    currentPlayerIndex = STARTING_PLAYER_INDEX;
+  }
+
+  public void startGame() {
+    currentPlayerIndex = 0;
+  }
+
+  public Player getCurrentPlayer() {
+    if (players == null || players.isEmpty()) {
+      throw new IllegalStateException("there must be at least one player");
     }
 
-    public void startGame() {
-      currentPlayerIndex = 0;
-    }
+    return players.get(currentPlayerIndex);
+  }
 
-    public Player getCurrentPlayer() {
-        if(players == null || players.isEmpty()) {
-            throw new IllegalStateException("there must be at least one player");
-        }
-
-        return players.get(currentPlayerIndex);
-    }
+  private boolean scoreQualifiesForWin(int score) {
+    return score >= PropertiesManager.getPointsReqForWin();
+  }
 
   public boolean gameHasStarted() {
-    return currentPlayerIndex >= 0;
+    return currentPlayerIndex > STARTING_PLAYER_INDEX;
+  }
+
+  public boolean someoneHasReachedWinningScore() {
+    for (Player player : playerScores.keySet()) {
+      if (scoreQualifiesForWin(playerScores.get(player))) {
+        return true;
+      }
+    }
+
+    return false;
   }
 
   public Player getNextPlayer() {
-    if(!gameHasStarted()) {
+    if (!gameHasStarted()) {
       throw new IllegalStateException("Game hasnt started");
     }
 
-    if(currentPlayerIndex == players.size() - 1) {
-      currentPlayerIndex = 0;
+    if (currentPlayerIndex == players.size() - 1) {
+      if (someoneHasReachedWinningScore()) {
+        return null;
+      }
+      else {
+        currentPlayerIndex = STARTING_PLAYER_INDEX + 1;
+      }
     }
     else {
       currentPlayerIndex++;
@@ -50,25 +71,20 @@ public class PlayerManager {
     return getCurrentPlayer();
   }
 
-    public void addPlayer(Player player) {
-        if(player == null) {
-            throw new IllegalArgumentException("Player cannot be null");
-        }
-
-        players.add(player);
-        playerScores.put(player, 0);
+  public void addPlayer(Player player) {
+    if (player == null) {
+      throw new IllegalArgumentException("Player cannot be null");
     }
 
-    public void addScore(int score) {
-        if(score < 0) {
-            throw new IllegalArgumentException("score cant be less than zero");
-        }
+    players.add(player);
+    playerScores.put(player, 0);
+  }
 
-
-        playerScores.put(getCurrentPlayer(), playerScores.get(getCurrentPlayer()) + score);
-
-        if(playerScores.get(getCurrentPlayer()) >= PropertiesManager.getPointsReqForWin()) {
-            // put logic here......
-        }
+  public void addScore(int score) {
+    if (score < PropertiesManager.getMinScore()) {
+      throw new IllegalArgumentException("score cant be less than minimum score");
     }
+
+    playerScores.put(getCurrentPlayer(), playerScores.get(getCurrentPlayer()) + score);
+  }
 }
---
+++ src/test/engine/PlayerManagerTest.java
@@ -0,0 +1,11 @@
+package test.engine;
+
+import org.testng.annotations.Test;
+
+public class PlayerManagerTest {
+
+  @Test
+  public void test() {
+
+  }
+}
